66f48d6ec5b4f0d6a2b6d5ad83021b46020be194
Denner Rondinely <denner.rondinely@gmail.com>
2025-06-10 18:54:42 -0300

    feat: create component menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 export function App() {
   return (
-    <Container>
-      <Logo />
-    </Container>
+    <>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+    </>
   );
 }
