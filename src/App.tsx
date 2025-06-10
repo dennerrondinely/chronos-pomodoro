5781cb7651967ef43e01e41438a56339ac532c51
Denner Rondinely <denner.rondinely@gmail.com>
2025-06-10 17:01:40 -0300

    feat: create component heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { TimerIcon } from "lucide-react";
+import { Heading } from "./components/Heading"
+
 export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My React App</h1>
+        <Heading>
+          Welcome to My React App
+          <button><TimerIcon /></button>
+        </Heading>
         <p>This is a simple React application.</p>
       </header>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 
 import { App } from './App'
 
+import './styles/theme.css'
+import './styles/global.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
