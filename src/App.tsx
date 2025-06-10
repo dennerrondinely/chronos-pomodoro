d0efd161022e3f95f4926e38326c7b5a8869e579
Denner Rondinely <denner.rondinely@gmail.com>
2025-06-10 17:27:27 -0300

    feat: create component container

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { TimerIcon } from "lucide-react";
-import { Heading } from "./components/Heading"
-
 export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Heading>
-          Welcome to My React App
-          <button><TimerIcon /></button>
-        </Heading>
-        <p>This is a simple React application.</p>
-      </header>
+    <div className="container">
+      <div className="content">
+        <section></section>
+      </div>
     </div>
   );
 }
